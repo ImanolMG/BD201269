990bbbcf7aad969d8bddc0c5bc6fb96260eb3452
Eduardo-201258 <79594431+Eduardo-201258@users.noreply.github.com>
2021-02-26 15:26:19 -0600

    Avance Eduardo

--- src/persistencia/CitaDAO.java
+++ src/persistencia/CitaDAO.java
@@ -1,4 +1,39 @@
 package persistencia;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+import java.util.*;
+
 public class CitaDAO {
+
+    private static SessionFactory factory;
+    private static ServiceRegistry serviceRegistry;
+
+    public CitaDAO() {
+        System.err.println("Iniciando conexionn");
+        try {
+            Configuration configuration = new Configuration();
+            System.err.println("Leyendo configuracion.");
+            configuration.configure();
+            serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();
+            factory = configuration.buildSessionFactory(serviceRegistry);
+        } catch (Throwable ex) {
+            System.err.println("No se puede crear la Sesion" + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public void getCitas() {
+        Session session = factory.openSession();
+        List empList1 = session.createQuery(" from Cita").list();
+
+        System.out.println("IdCita \t Nombre Mascota \t Fecha \t Servicio");
+        for (Iterator iterator = empList1.iterator(); iterator.hasNext();){
+            Cita dao = (Cita) iterator.next();
+            System.out.println( dao.getId()+ "\t" + dao.getNombreMascota()+"\t" + dao.getFecha() +"\t"+ dao.getServicio());
+        }
+    }
 }
--- src/persistencia/Dueño.java
+++ src/persistencia/Dueño.java
@@ -1,4 +1,48 @@
 package persistencia;
 
-public class Dueño {
+import java.io.Serializable;
+
+public class Dueño implements Serializable {
+
+    private String Nombre;
+    private String Direccion;
+    private Integer Numero;
+    private Dueño dueño;
+    public Dueño() {
+    }
+
+    public Dueño(String Nombre,String Direccion,Integer Numero) {
+        this.Nombre = Nombre;
+        this.Direccion = Direccion;
+        this.Numero = Numero;
+    }
+
+    public void setNombre(String Nombre){
+        this.Nombre = Nombre;
+    }
+    public String getNombre(){
+        return Nombre;
+    }
+
+    public void setDireccion(String Direccion){
+        this.Direccion=Direccion;
+    }
+    public String getDireccion(){
+        return Direccion;
+    }
+
+    public void setNumero(Integer Numero){
+        this.Numero=Numero;
+    }
+    public Integer getNumero(){
+        return Numero;
+    }
+
+
+    public void setDueño(Dueño dueño) {
+        this.dueño = dueño;
+    }
+    public Dueño getDueño() {
+        return dueño;
+    }
 }
