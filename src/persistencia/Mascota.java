39233a203e08df660d6827c3ec3d00ce289c8888
201269ImanolMG <201269@ids.upchiapas.edu.mx>
2021-02-26 20:13:52 -0600

    Avance Imanol

--- src/persistencia/Mascota.java
+++ src/persistencia/Mascota.java
@@ -1,4 +1,87 @@
 package persistencia;
 
 public class Mascota {
+
+    private Integer IdMascota;
+    private String TipoMascota;
+    private String Nombre;
+    private String FechaIngreso;
+    private Integer IdDueño;
+    private String Sexo;
+    private String Motivo;
+
+
+    public Mascota(Integer idMascota, String tipoMascota, String nombre, String fechaIngreso, Integer idDueño, String sexo, String motivo) {
+        IdMascota = idMascota;
+        TipoMascota = tipoMascota;
+        Nombre = nombre;
+        FechaIngreso = fechaIngreso;
+        IdDueño = idDueño;
+        Sexo = sexo;
+        Motivo = motivo;
+    }
+
+
+    public Integer getIdMascota() {
+        return IdMascota;
+    }
+
+    public Mascota setIdMascota(Integer idMascota) {
+        IdMascota = idMascota;
+        return this;
+    }
+
+    public String getTipoMascota() {
+        return TipoMascota;
+    }
+
+    public Mascota setTipoMascota(String tipoMascota) {
+        TipoMascota = tipoMascota;
+        return this;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public Mascota setNombre(String nombre) {
+        Nombre = nombre;
+        return this;
+    }
+
+    public String getFechaIngreso() {
+        return FechaIngreso;
+    }
+
+    public Mascota setFechaIngreso(String fechaIngreso) {
+        FechaIngreso = fechaIngreso;
+        return this;
+    }
+
+    public Integer getIdDueño() {
+        return IdDueño;
+    }
+
+    public Mascota setIdDueño(Integer idDueño) {
+        IdDueño = idDueño;
+        return this;
+    }
+
+    public String getSexo() {
+        return Sexo;
+    }
+
+    public Mascota setSexo(String sexo) {
+        Sexo = sexo;
+        return this;
+    }
+
+    public String getMotivo() {
+        return Motivo;
+    }
+
+    public Mascota setMotivo(String motivo) {
+        Motivo = motivo;
+        return this;
+    }
 }
--- src/persistencia/Medicamento.java
+++ src/persistencia/Medicamento.java
@@ -1,5 +1,54 @@
 package persistencia;
 
-public class Medicamento {
+import java.io.Serializable;
 
+public class Medicamento implements Serializable {
+
+    private Integer Codigo;
+    private String NombreMedicamento;
+    private String SustanciaActiva;
+    private String Caducidad;
+
+    public Medicamento(Integer codigo, String nombreMedicamento, String sustanciaActiva, String caducidad) {
+        Codigo = codigo;
+        NombreMedicamento = nombreMedicamento;
+        SustanciaActiva = sustanciaActiva;
+        Caducidad = caducidad;
+    }
+
+    public Integer getCodigo() {
+        return Codigo;
+    }
+
+    public Medicamento setCodigo(Integer codigo) {
+        Codigo = codigo;
+        return this;
+    }
+
+    public String getNombreMedicamento() {
+        return NombreMedicamento;
+    }
+
+    public Medicamento setNombreMedicamento(String nombreMedicamento) {
+        NombreMedicamento = nombreMedicamento;
+        return this;
+    }
+
+    public String getSustanciaActiva() {
+        return SustanciaActiva;
+    }
+
+    public Medicamento setSustanciaActiva(String sustanciaActiva) {
+        SustanciaActiva = sustanciaActiva;
+        return this;
+    }
+
+    public String getCaducidad() {
+        return Caducidad;
+    }
+
+    public Medicamento setCaducidad(String caducidad) {
+        Caducidad = caducidad;
+        return this;
+    }
 }
