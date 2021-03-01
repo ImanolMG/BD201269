8950c71bea9e5e765b96597192df2741b6cb1b5f
201269ImanolMG <201269@ids.upchiapas.edu.mx>
2021-02-28 19:43:01 -0600

    Avance Imanol Alonso Eduardo

--- src/vistas/SignUp.java
+++ src/vistas/SignUp.java
@@ -1,6 +1,25 @@
 package vistas;
 
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class SignUp {
+
+    @FXML
+    private Button btnCancelar;
+
+
+
+
+    @FXML
+    private void cancelarRegistro(ActionEvent event) {
+        Node source = (Node) event.getSource();
+        Stage stage = (Stage) source.getScene().getWindow();
+        stage.close();
+    }
 
 }
--- src/vistas/login.java
+++ src/vistas/login.java
@@ -2,11 +2,16 @@ package vistas;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 import persistencia.Usuarios;
 import persistencia.UsuariosDAO;
 import javax.swing.*;
+import java.io.IOException;
 import java.util.*;
 
 public class login {
@@ -16,6 +21,9 @@ public class login {
     private PasswordField idPass;
     @FXML
     private Button btnLogin;
+    @FXML
+            private  Button btnSignUp;
+
     UsuariosDAO view = new UsuariosDAO();
 
     @FXML
@@ -26,6 +34,23 @@ public class login {
         evaluarInicio = view.BuscarUsuario(idUser.getText(),idPass.getText());
         if(evaluarInicio==false){
             JOptionPane.showMessageDialog(null, "Usuario o contraseña incorrectos", "No se puedo iniciar la sesion",JOptionPane.WARNING_MESSAGE);
+        }
+    }
+
+@FXML
+    public void handlerVistaSignUp() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("signUp.fxml"));
+            AnchorPane page = (AnchorPane) loader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(page));
+            stage.setTitle("Caja");
+            stage.show();
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+
         }
     }
 }
