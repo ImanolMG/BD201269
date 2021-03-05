9c3566c85668dbb5d9926d020bd780c4c7279d88
AAGS589 <68868495+AAGS589@users.noreply.github.com>
2021-03-04 21:40:50 -0600

    04/03/2021

---
+++ src/vistas/AccionesUsuarios.java
@@ -0,0 +1,23 @@
+package vistas;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class AccionesUsuarios {
+
+    @FXML
+    private Button idNew;
+    @FXML
+    private Button idGuardar;
+    @FXML
+    private Button idEditar;
+    @FXML
+    private Button idEliminar;
+    @FXML
+    private TextField idNombre;
+    @FXML
+    private TextField idDireccion;
+    @FXML
+    private TextField idTelefono;
+}
